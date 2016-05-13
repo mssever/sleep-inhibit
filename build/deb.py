3d3e290a1fcba92c742f2df3169150beb0f97f42
Scott Severance <scott@scottseverance.us>
2016-05-13 01:35:04 -0500

    The script to create the .deb is roughed out. The files under build/deb_files need to be edited. The whole shebang is totally untested and certainly contains many bugs.

--- build/deb.py
+++ build/deb.py
@@ -12,13 +12,37 @@ def main(basedir):
     config = get_settings()
     builddir = join(basedir, 'builddir')
     srcdir = join(builddir, '{}-{}'.format('sleep-inhibit', config.version))
+    staticdir = join(basedir, 'build/deb_files')
+    srcfiles = [join(basedir, 'LICENSE'), join(basedir, 'README.md'),
+                join(basedir, 'sleepinhibit'), join(basedir, 'sleep_inhibit.py')]
+    debiandir = join(srcdir, 'debian')
+    installdir = join(debiandir, 'install')
 
-    cmd_output([join(basedir, 'cleanup.py'), '--run-from-script'])
+    print(cmd_output([join(basedir, 'cleanup.py'), '--run-from-script']))
     os.mkdir(builddir)
     os.mkdir(srcdir)
-    cmd_output(['cp', '-r', join(basedir, 'LICENSE'), join(basedir, 'README.md'),
-                join(basedir, 'sleepinhibit'), join(basedir, 'sleep_inhibit.py'),
-                srcdir])
+    print(cmd_output(['cp', '-r'] + srcfiles + [srcdir]))
     os.chdir(srcdir)
     print(cmd_output(['dh_make', '--indep', '--createorig', '-cgpl3',
                 '--packagename=sleep-inhibit', '-y']))
+    static_files = os.listdir(staticdir)
+    for file_ in os.listdir(debiandir):
+        if file_.startswith('README') or file_.endswith('.ex') or file_.endswith('.EX') or file_ in static_files:
+            os.unlink(file_)
+    for index, dir_ in enumerated(('usr/lib/python3/dist-packages/sleep-inhibit', 'usr/share/applications', 'usr/bin')):
+        os.mkdirs(join(installdir, dir_))
+        if index == 0:
+            print(cmd_output(['cp', '-r'] + srcfiles + [dir_]))
+        elif index == 1:
+            path = 'usr/lib/python3/dist-packages/sleep-inhibit'
+            with open(join(basedir, 'sleepinhibit/data/sleep-inhibit.desktop.template')) as src:
+                with open(join(installdir, dir_, 'sleep-inhibit.desktop'), 'w') as dest:
+                    dest.write(src.read().format(programpath=join(path, 'sleep_inhibit.py'),
+                                                 iconpath=join(path, 'sleepinhibit/img/window_icon.png'))
+            os.chmod(join(installdir, dir_, 'sleep-inhibit.desktop'), 0o755)
+        elif index == 2:
+            print(cmd_output(['cp', '-r', join(staticdir, 'sleep-inhibit'), join(installdir, 'usr/bin')]))
+        else:
+            raise RuntimeError('Iterating over the wrong number of directories')
+    print(cmd_output(['cp', '-r'] + [join(staticdir, file_) for file_ in static_files if file_ != 'sleep-inhibit'] + [debiandir]))
+    print(cmd_output(['debuild']))
